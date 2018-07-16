12ff2bce76f809f04f0efacc45fbc7f48e51ec17
Jen Wachter <jenleighkelly@gmail.com>
2018-07-16 10:34:21 -0400

    Scroll the user back to the top of the results when filtering is triggered

--- views/ContainerView.js
+++ views/ContainerView.js
@@ -37,6 +37,8 @@ module.exports = Backbone.View.extend({
 
   fetchData: function (state) {
 
+    this.scrollUser();
+
     var data = {};
 
     $.each(state, function (key, value) {
@@ -58,6 +60,47 @@ module.exports = Backbone.View.extend({
 
     });
 
+  },
+
+  /**
+   * Scroll the user back to top of the container if they
+   * are below the container
+   * @return null
+   */
+  scrollUser: function () {
+
+    // how far the user is scrolled
+    var documentOffset = $(document).scrollTop();
+
+    // how far down the container is
+    var elementOffset = this.$el.offset().top;
+
+    // how far below the container the user is scrolled
+    var belowContainer = documentOffset - elementOffset;
+
+    // how tall the user's window it
+    var windowHeight = $(window).height();
+
+    // if user is scrolled below the container more than the height of their window / 3
+    if (documentOffset > elementOffset && belowContainer > (windowHeight / 3)) {
+
+      // scroll the user back to the top
+      $('html, body').animate({
+        scrollTop: this.$el.offset().top
+      }, belowContainer);
+
+    }
+
+  },
+
+  activate: function () {
+    this.cover.hide();
+    this.$el.removeClass('data-loading');
+  },
+
+  deactivate: function () {
+    this.cover.show();
+    this.$el.addClass('data-loading');
   },
 
   render: function () {
